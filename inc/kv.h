54da0d6a2ffbe4e3356d9bdfbad238d174105150
João Amgarten <joaoamgarten2003@gmail.com>
2026-04-15 00:45:52 +0100

    fix typo

--- inc/kv.h
+++ inc/kv.h
@@ -15,7 +15,7 @@ typedef struct {
   kv_entry_t *entries;
 } kv_t;
 
-kv_t *kv_init(size_t capcaity);
+kv_t *kv_init(size_t capacity);
 int kv_put(kv_t *db, const char *key, const char *value);
 char *kv_get(kv_t *db, const char *key);
 int kv_delete(kv_t *db, const char *key);
